7a81b47f904bc86a799885d1b7bc1850b97728df
Nikolay Tankov <tankov.n.k@gmail.com>
2022-09-25 13:50:06 +0300

    theme complite

--- src/components/Constans/theme.js
+++ src/components/Constans/theme.js
@@ -7,4 +7,29 @@ export const theme = {
     gray: '#aaa',
     red: 'rgb(200, 19, 19)',
   },
+  space: [0, 2, 4, 8, 16, 32, 64, 128, 256],
+  fontSizes: {
+    xs: '12px',
+    s: '14px',
+    m: '16px',
+    l: '32px',
+    xl: '64px',
+  },
+  fontWeights: {
+    normal: 400,
+    bold: 700,
+  },
+  lineHeights: {
+    body: 1.5,
+    heading: 1.125,
+  },
+  borders: {
+    none: 'none',
+    normal: '1px solid',
+  },
+  radii: {
+    none: '0',
+    normal: '4px',
+    round: '50%',
+  },
 };
