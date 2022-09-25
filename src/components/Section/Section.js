750f00a8586150d893bad9ae74ca98398602bf02
Nikolay Tankov <tankov.n.k@gmail.com>
2022-09-25 15:37:05 +0300

    correct container

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -1,17 +1,11 @@
 import PropTypes from 'prop-types';
 
 // передаем в секцию детей
-function Section({ title, children }) {
-  return (
-    <div>
-      {<h2>{title}</h2>}
-      {children}
-    </div>
-  );
+function Section({ children }) {
+  return <div>{children}</div>;
 }
 
 Section.propTypes = {
-  title: PropTypes.string,
   //   значение что угодно (что-то должно быть)
   children: PropTypes.node,
 };
