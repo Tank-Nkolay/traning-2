1632f35166fa85107e3590eb7c90085ea5972ec5
Nikolay Tankov <tankov.n.k@gmail.com>
2022-09-25 19:43:19 +0300

    border

--- src/components/TransactionHistory/TransactionItem.styled.jsx
+++ src/components/TransactionHistory/TransactionItem.styled.jsx
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled';
 
 export const Td = styled.td`
-border: 1px solid ${props => props.theme.colors.grayLight}};
+  border: 1px solid ${props => props.theme.colors.grayLight};
   font-size: 24px;
 `;
