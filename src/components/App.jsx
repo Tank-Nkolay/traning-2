e5d201ee9ef3f17fd23f667eab08860c9ce860c6
Nikolay Tankov <tankov.n.k@gmail.com>
2022-09-28 19:13:34 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,8 @@
 // импорт масива данных для разметки (для пропсов)
-import user from '../json/user.json';
-import data from '../json/data.json';
 import friends from '../json/friends.json';
 import items from '../json/transactions.json';
 // импорт функции разметки
 import Section from './Section/Section';
-import Profile from './Profile/Profile';
-import Statistics from './Statistics/Statistics';
 import FriendList from './FriendList/FriendList';
 import TransactionHistory from './TransactionHistory/TransactionHistory';
 // =========================================================
@@ -26,16 +22,6 @@ export const App = () => {
     >
       <Section>
         <Feedback />
-        <Profile
-          username={user.username}
-          tag={user.tag}
-          location={user.location}
-          avatar={user.avatar}
-          followers={user.stats.followers}
-          views={user.stats.views}
-          likes={user.stats.likes}
-        />
-        <Statistics title="UPLOAD STATS" stats={data} />
         <FriendList friendsData={friends} />
         <TransactionHistory transactionData={items} />
       </Section>
