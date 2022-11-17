b04ce1ca4d76dabdec510b91862f7709f8449814
Nikolay Tankov <tankov.n.k@gmail.com>
2022-11-17 22:09:57 +0200

    Section

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,5 @@
-import { MainBox } from './App.styled';
+import Section from '../Section/Section';
+import { Markup } from './App.styled';
 import FormEl from 'components/Form/Form';
 import ContactList from 'components/ContactList/ContactList';
 import Filter from 'components/Filter';
@@ -6,11 +7,13 @@ import Title from 'components/Title/Title';
 
 export default function App() {
   return (
-    <MainBox>
-      <Title title="Phonebook" />
-      <FormEl />
-      <Filter title="Find contacts by name" />
-      <ContactList title="Contacts" />
-    </MainBox>
+    <Section>
+      <Markup>
+        <Title title="Phonebook" />
+        <FormEl />
+        <Filter title="Find contacts by name" />
+        <ContactList title="Contacts" />
+      </Markup>
+    </Section>
   );
 }
