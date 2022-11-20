96d99c2069483bbe0f4acad9fe62488bd6e9fda5
Nikolay Tankov <tankov.n.k@gmail.com>
2022-11-20 20:59:35 +0200

    filter

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,8 +1,10 @@
 import { PropTypes } from 'prop-types';
-import { FindWrapper, FindTitle, Input } from './Filter.styled';
 import { useSelector, useDispatch } from 'react-redux';
+// ====================================================
+import { Container, Title, Input } from './Filter.styled';
 import { setFilter } from 'redux/filterSlice';
 import { getFilter } from 'redux/selectors';
+// ====================================================
 
 export default function Filter({ title }) {
   const dispatch = useDispatch();
@@ -12,16 +14,16 @@ export default function Filter({ title }) {
     dispatch(setFilter(event.currentTarget.value));
   };
   return (
-    <FindWrapper>
-      <FindTitle>{title}</FindTitle>
+    <Container>
+      <Title>Contacts</Title>
       <Input
         type="text"
         name="filter"
-        placeholder="Enter search name"
+        placeholder="Find contacts by name"
         value={filter.value}
         onChange={onFilterChange}
       />
-    </FindWrapper>
+    </Container>
   );
 }
 
---
+++ src/components/Filter/Filter.styled.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  margin-top: 20px;
+`;
+
+export const Title = styled.h3``;
+
+export const Input = styled.input`
+  width: 95%;
+  padding: 5px 10px 5px 10px;
+  font-size: 16px;
+`;
