723e77b8a0b0af4c9c3e86b707652b580411bed6
Nikolay Tankov <tankov.n.k@gmail.com>
2022-12-01 13:00:33 +0200

    home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,29 +1,19 @@
-import { Text } from '@chakra-ui/react';
-const styles = {
-  container: {
-    height: '100vh',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontWeight: 500,
-    fontSize: 28,
-    textAlign: 'center',
-    marginBottom: '8px',
-  },
-};
+import { Flex, Text } from '@chakra-ui/react';
 
 export default function Home() {
   return (
-    <div style={styles.container}>
+    <Flex
+      h="100vh"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+    >
       <Text fontSize="6xl" color="#0e0f69">
         Your best phonePhoneBook
       </Text>
       <Text fontSize="4xl" color="red">
         *to use - Register
       </Text>
-    </div>
+    </Flex>
   );
 }
