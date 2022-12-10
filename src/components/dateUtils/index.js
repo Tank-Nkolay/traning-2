0276f89d800a073e84a91180988f30d6eb38a72d
Nikolay Tankov <tankov.n.k@gmail.com>
2022-12-10 14:52:55 +0200

    sum

--- src/components/dateUtils/index.js
+++ src/components/dateUtils/index.js
@@ -4,8 +4,15 @@ console.log(`get current date: ${getCurrentDate}`);
 
 const Calc = require('calc-js').Calc;
 
+// ==================
 console.log(process.argv);
+const [, , a, b] = process.argv;
 
-console.log(new Calc(0.2).sum(0.1).finish());
+console.log(new Calc(parseInt(a)).sum(parseInt(b)).finish());
 
 // node src/components/dateUtils/index
+
+// ==================
+// console.log(process.argv);
+// console.log(new Calc(0.2).sum(0.1).finish());
+// ==================
