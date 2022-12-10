fd31163dfb199971ca9cc2c0d35caea6edfe70db
Nikolay Tankov <tankov.n.k@gmail.com>
2022-12-10 14:45:42 +0200

    calc

--- src/components/dateUtils/index.js
+++ src/components/dateUtils/index.js
@@ -1,3 +1,11 @@
 const { getCurrentDate } = require('./DateUtils');
 
 console.log(`get current date: ${getCurrentDate}`);
+
+const Calc = require('calc-js').Calc;
+
+console.log(process.argv);
+
+console.log(new Calc(0.2).sum(0.1).finish());
+
+// node src/components/dateUtils/index
