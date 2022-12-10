7f542e390539c84039ab51fafef81b454979d099
Nikolay Tankov <tankov.n.k@gmail.com>
2022-12-10 15:02:11 +0200

    calc 2

--- src/components/dateUtils/index.js
+++ src/components/dateUtils/index.js
@@ -6,7 +6,9 @@ const Calc = require('calc-js').Calc;
 
 // ==================
 console.log(process.argv);
-const [, , a, b] = process.argv;
+// const [, , a, b] = process.argv;
+const a = process.argv[2];
+const b = process.argv[3];
 
 console.log(new Calc(parseInt(a)).sum(parseInt(b)).finish());
 
