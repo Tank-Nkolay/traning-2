ffeef38723aa3fd5f6285832b74696169b394425
Nikolay Tankov <tankov.n.k@gmail.com>
2022-12-10 18:40:32 +0200

    path

--- src/components/dateUtils/index.js
+++ src/components/dateUtils/index.js
@@ -2,16 +2,20 @@ const { getCurrentDate } = require('./DateUtils');
 
 console.log(`get current date: ${getCurrentDate}`);
 
-const Calc = require('calc-js').Calc;
+// const Calc = require('calc-js').Calc;
+const path = require('path');
 
+// математика ==================
+// console.log(process.argv);
+// // const [, , a, b] = process.argv;
+// const a = process.argv[2];
+// const b = process.argv[3];
+// console.log(new Calc(parseInt(a)).sum(parseInt(b)).finish());
+// посмотреть путь ==================
+console.log(path.resolve('dataUtils.js'));
 // ==================
-console.log(process.argv);
-// const [, , a, b] = process.argv;
-const a = process.argv[2];
-const b = process.argv[3];
 
-console.log(new Calc(parseInt(a)).sum(parseInt(b)).finish());
-
+// вывести в консоль ===
 // node src/components/dateUtils/index
 
 // ==================
