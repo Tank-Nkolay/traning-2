c2b12edc0fd7590f82638e64c61ce06bb32ab934
Nikolay Tankov <tankov.n.k@gmail.com>
2022-12-18 16:18:16 +0200

    app

---
+++ src/components/dateUtils/app.js
@@ -0,0 +1,13 @@
+// вывести в консоль ===
+// node src/components/dateUtils/app
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
