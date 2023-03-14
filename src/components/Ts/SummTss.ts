c139f86fcde0300ebb42ad91b7a28e62eb487079
Nikolay Tankov <tankov.n.k@gmail.com>
2023-03-14 22:33:28 +0200

    ts

--- src/components/Ts/SummTss.ts
+++ src/components/Ts/SummTss.ts
@@ -13,3 +13,46 @@ button.addEventListener('click', function () {
 let foo: number[] = [];
 
 foo = [1];
+
+let age = 50;
+let name = 'Max';
+let toggle = true;
+let empty = null;
+let notInitialize = undefined;
+let callback = a => {
+  return 100 + a;
+};
+let anything = -20;
+anything = 'Text';
+anything = {};
+let person = ['Max', 21];
+
+function showMessage(message) {
+  console.log(message);
+}
+
+function calc(num1, num2) {
+  return num1 + num2;
+}
+
+function customError() {
+  throw new Error('Error');
+}
+
+const page1 = {
+  title: 'The awesome page',
+  likes: 100,
+  accounts: ['Max', 'Anton', 'Nikita'],
+  status: 'open',
+  details: {
+    createAt: '2021-01-01',
+    updateAt: '2021-05-01',
+  },
+};
+
+const page2 = {
+  title: 'Python or Js',
+  likes: 5,
+  accounts: ['Alex'],
+  status: 'close',
+};
